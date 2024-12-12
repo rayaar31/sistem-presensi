a5d7deb8cd25f7213410b067ad629c987d32b9c8
rayaar31 <rayaar07@gmail.com>
2024-12-12 19:15:52 +0700

    Update db.php

--- sistem_presensi/db.php
+++ sistem_presensi/db.php
@@ -1,13 +1,23 @@
 <?php
-$db = parse_url(getenv("DATABASE_URL"));
-$host = $db["host"];
-$port = $db["port"];
-$user = $db["user"];
-$pass = $db["pass"];
-$dbname = ltrim($db["path"], '/');
+session_start();
 
-$conn = new mysqli($host, $user, $pass, $dbname, $port);
+$databaseUrl = getenv('DATABASE_URL');
+if (!$databaseUrl) {
+    die("Error: DATABASE_URL tidak ditemukan.");
+}
 
+$parsedUrl = parse_url($databaseUrl);
+
+// Ambil detail koneksi dari DATABASE_URL
+$db_host = $parsedUrl["host"];
+$db_user = $parsedUrl["user"];
+$db_pass = $parsedUrl["pass"];
+$db_name = ltrim($parsedUrl["path"], '/');
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+// Cek koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
+?>
