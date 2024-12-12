30455aa3268042379dc3d1e04dfdc81ba6c4ec3d
rayaar31 <rayaar07@gmail.com>
2024-12-13 02:00:25 +0700

    Update db.php

--- sistem_presensi/db.php
+++ sistem_presensi/db.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php'; // Memuat autoloader Composer
+// require_once __DIR__ . '/vendor/autoload.php'; // Memuat autoloader Composer
 
 // Memuat file .env
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
